9499fbecdff3b94bed1b015f3983cdc209874a8e
Daniel Konovalenko <ddkono@gmail.com>
2016-03-18 17:38:08 +0300

    Added request to database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, request
 from .forms import IDForm
+from app import models
 
 
 @app.route('/')
@@ -12,5 +13,9 @@ def index():
 
 @app.route('/results', methods=['POST'])
 def results():
+    # TODO: Отдавать только имеющиеся результаты
     if request.method == 'POST':
-        return render_template('results.html')
+        student_id = request.form['id']
+        student_results = models.Results.query.get(student_id)
+        print(results)
+        return render_template('results.html', results=student_results)
