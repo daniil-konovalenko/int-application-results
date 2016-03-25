750d1ecb1078664c27d0a356521f0e29ec9df488
Daniel Konovalenko <ddkono@gmail.com>
2016-03-25 21:38:03 +0300

    Added max scores to subjects

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,11 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 subjects = {
-    "math_test": "Математика (тест)",
-    "math": "Математика",
-    "philology": "Филология",
-    "history": "История",
-    "science": "Естествознание",
+    "math_test": {"name": "Математика (тест)", "max_score": 13},
+    "math": {"name": "Математика", "max_score": 20},
+    "philology": {"name": "Филология", "max_score": 20},
+    "history": {"name": "История", "max_score": 20},
+    "science": {"name": "Естествознание", "max_score": 20},
 }
 
 app = Flask(__name__.split('.')[0])
