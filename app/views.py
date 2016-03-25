3826fd7bc7336df0df079057ab9b1b91e5bcb974
Daniel Konovalenko <ddkono@gmail.com>
2016-03-25 22:19:28 +0300

    Fixed final value

--- app/views.py
+++ app/views.py
@@ -33,9 +33,16 @@ def results():
                          for column in student_results.__table__.columns.keys()
                          if student_results[column] is not None if column in subjects]
 
+        normalized_math_test = student_results.math_test / 13 * 20
+        sum_of_two_best = sum(sorted([result[1] for result in results_table[1:]],
+                                     reverse=True)[:2])
+
+        final = "{:.1f}".format(normalized_math_test + sum_of_two_best)
+
         return render_template('results.html',
                                table=results_table,
                                student_id=student_id,
+                               final=final
                                )
 
     else:
