e8d6e1412db49d95f1565dd285fe77357483606b
Daniel Konovalenko <ddkono@gmail.com>
2016-04-04 22:11:06 +0300

    Extracted final score calculation to a separate function

--- calculate_final.py
+++ calculate_final.py
@@ -6,29 +6,32 @@ import logging
 
 math_max = 13
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    for result in models.Results.query.all():
-        logging.info("Procesing ", result)
-
-        results_table = [(subjects.get(column).get('name'),
+def final_score(result):
+    results_table = [(subjects.get(column).get('name'),
                         result[column],
                           subjects.get(column).get('max_score'))
                           for column in result.__table__.columns.keys()
                           if result[column] is not None if column in subjects]
 
-        try:
-            normalized_math_test = result.math_test / math_max * 20
-            sum_of_two_best = sum(sorted([result[1] for result in results_table[1:]],
+    normalized_math_test = result.math_test / math_max * 20
+    sum_of_two_best = sum(sorted([result[1] for result in results_table[1:]],
                                      reverse=True)[:2])
 
-            final = round(sum_of_two_best + normalized_math_test, 1)
+    final = round(sum_of_two_best + normalized_math_test, 1)
+    return final
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    for result in models.Results.query.all():
+        logging.info("Procesing ", result)
+
+
+        try:
+
+            final = final_score(result)
             result.final = final
-
-
         except TypeError as e:
             logging.error(e)
-            logging.error(results_table)
-
     db.session.commit()
     logging.info("Finished")
