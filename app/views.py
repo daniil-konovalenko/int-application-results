38bbe9b08615a232497b45ae93c3fdeaa0be76b7
Daniel Konovalenko <ddkono@gmail.com>
2016-04-04 22:50:17 +0300

    Removed calculating final score from results view, added floats round

--- app/views.py
+++ app/views.py
@@ -28,17 +28,13 @@ def results():
             return redirect(url_for('index'))
 
         results_table = [(subjects.get(column).get('name'),
-                          student_results[column],
+                          round(student_results[column], 1),
                           subjects.get(column).get('max_score'))
                          for column in student_results.__table__.columns.keys()
                          if student_results[column] is not None if column in subjects]
 
-        normalized_math_test = student_results.math_test / 13 * 20
-        sum_of_two_best = sum(sorted([result[1] for result in results_table[1:]],
-                                     reverse=True)[:2])
 
-        final = "{:.1f}".format(normalized_math_test + sum_of_two_best)
-
+        final = round(student_results.final, 1)
         return render_template('results.html',
                                table=results_table,
                                student_id=student_id,
