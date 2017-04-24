b0cc13f9286dcae26a75c4824e85fe19191443d8
Daniel Konovalenko <ddkono@gmail.com>
2017-04-24 19:47:13 +0300

    Extract results table fetching from database into a separate function

--- web/app/views.py
+++ web/app/views.py
@@ -1,13 +1,17 @@
 from app import app
+from app import subjects
 from flask import (render_template,
                    request,
                    redirect,
                    url_for,
                    flash,
                    )
-from .forms import IDForm
+
 from app import models
-from app import subjects
+from .forms import IDForm
+
+from collections import namedtuple
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,26 +24,39 @@ def index():
 def results():
     if request.method == 'POST':
         student_id = request.form['id']
-        student_results = models.Results.query.get(student_id)
-
-        if not student_results:
-            flash('Кажется, участника с таким номером у нас нет. Попробуйте еще раз',
+        results = get_results(student_id)
+        
+        if results is None:
+            flash('Участника с таким номером у нас нет. Попробуйте еще раз',
                   'warning')
             return redirect(url_for('index'))
-
-        results_table = [(subjects.get(column).get('name'),
-                          round(student_results[column], 1),
-                          subjects.get(column).get('max_score'))
-                         for column in student_results.__table__.columns.keys()
-                         if student_results[column] is not None if column in subjects]
-
-
-        final = round(student_results.final, 1)
+        
         return render_template('results.html',
-                               table=results_table,
+                               table=results.table,
                                student_id=student_id,
-                               final=final
+                               final=results.final
                                )
-
     else:
         return redirect(url_for('index'))
+
+
+def get_results(student_id):
+    student_results = models.Results.query.get(student_id)
+    
+    if not student_results:
+        return None
+    
+    # Oh god why
+    # TODO: make readable
+    results_table = [(subjects.get(column).get('name'),
+                      round(student_results[column], 1),
+                      subjects.get(column).get('max_score'))
+                     for column in student_results.__table__.columns.keys()
+                     if student_results[column] is not None if
+                     column in subjects]
+    
+
+    
+    final = round(student_results.final, 1)
+    results = namedtuple('results', ['table', 'final'])
+    return results(results_table, final)
