1d889997463049ffcd94c8247d1ac49a5968b6c1
Daniel Konovalenko <ddkono@gmail.com>
2017-04-25 13:59:58 +0300

    Add gunicorn logging

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+import logging
 
 subjects = {
     "math_test": {"name": "Математика (тест)", "max_score": 13},
@@ -27,6 +28,9 @@ subjects = {
 app = Flask(__name__.split('.')[0])
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
 
 migrate = Migrate(app, db)
 
