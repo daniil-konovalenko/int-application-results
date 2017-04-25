c2066be6cdb9b8945fcacc545d2cf78baf9af994
Daniel Konovalenko <ddkono@gmail.com>
2017-04-25 18:58:20 +0300

    Remove code that used old DB schema

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -3,26 +3,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-subjects = {
-    "math_test": {"name": "Математика (тест)", "max_score": 13},
-    "math_test_18": {"name": "Математика (тест)", "max_score": 18},
-    "math": {"name": "Математика", "max_score": 20},
-    "philology": {"name": "Филология", "max_score": 20},
-    "history": {"name": "История", "max_score": 20},
-    "science": {"name": "Естествознание", "max_score": 20},
-    "biology": {"name": "Биология", "max_score": 20},
-    "chemistry": {"name": "Химия", "max_score": 20},
-    "english": {"name": "Английский язык", "max_score": 20},
-    "geography": {"name": "География", "max_score": 20},
-    "russian": {"name": "Русский язык", "max_score": 20},
-    "math_individual_plan": {"name": "Математика (на программу с единым учебным планом)",
-                             "max_score": 20},
-    "math_unified_plan":
-        {"name": "Математика (на программу с индивидуальным учебным планом)",
-         "max_score": 20}
-    
-
-}
 
 app = Flask(__name__.split('.')[0])
 app.config.from_object('config')
