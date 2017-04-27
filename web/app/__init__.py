096a8c41d946a71e82365aedf5f941806725ec62
Daniel Konovalenko <ddkono@gmail.com>
2017-04-28 00:42:49 +0300

    Add flask-admin views

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,14 +1,18 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+import os
+
+from flask import Flask, url_for
+from flask_admin import Admin
+from flask_admin import helpers as admin_helpers
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from flask_admin import Admin
-
-import logging
+from flask_security import Security, SQLAlchemyUserDatastore
+from flask_sqlalchemy import SQLAlchemy
+from flask_security.utils import encrypt_password
 
 
 app = Flask(__name__.split('.')[0])
-app.config.from_object('config')
+app.config.from_object(os.environ.get('APP_SETTINGS', 'config.DevelopmentConfig'))
 
 db = SQLAlchemy(app)
 
@@ -22,6 +26,42 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def create_superuser():
+    """Creates superuses in db"""
+    user_role = Role(name='user')
+    superuser_role = Role(name='superuser')
+    with app.app_context():
+        db.session.add(user_role)
+        db.session.add(superuser_role)
+        
+        superuser = user_datastore.create_user(
+            first_name='Admin',
+            email='admin',
+            password=encrypt_password('admin'),
+            roles=[user_role, superuser_role]
+        )
+        db.session.commit()
+    
+
+from app.models import User, Role
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+
+# define a context processor for merging flask-admin's template context into the
+# flask-security views.
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+        get_url=url_for
+    )
 
 
 from app import views, admin_views
--- web/app/admin_views.py
+++ web/app/admin_views.py
@@ -1,14 +1,40 @@
 from app import admin, db
 from flask_admin.contrib.sqla import ModelView
-from app.models import Subject, Student, Result
+from app.models import Subject, Student, Result, Role, User
+from flask_security import current_user
+from flask import redirect, url_for, request, abort
 
 
-class SubjectModelView(ModelView):
+class ProtectedModelView(ModelView):
+    def is_accessible(self):
+        if not current_user.is_active or not current_user.is_authenticated:
+            return False
+        if current_user.has_role('superuser'):
+            return True
+    
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                return redirect(url_for('/'))
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
+
+class SubjectModelView(ProtectedModelView):
     column_editable_list = ['name', 'max_score']
     create_modal = True
 
 
 
+
+admin.add_view(ProtectedModelView(Role, db.session))
+admin.add_view(ProtectedModelView(User, db.session))
+
 admin.add_view(SubjectModelView(Subject, db.session))
-admin.add_view(ModelView(Student, db.session))
-admin.add_view(ModelView(Result, db.session))
+admin.add_view(ProtectedModelView(Student, db.session))
+admin.add_view(ProtectedModelView(Result, db.session))
